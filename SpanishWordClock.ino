610d3d76d06398728aac790e163722b4fd74e63f
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2019-02-20 22:11:51 -0600

    fixed quince bug and month state bug

--- SpanishWordClock.ino
+++ SpanishWordClock.ino
@@ -18,7 +18,7 @@
 
 const int delaytime = 250;
 const uint16_t NUM_LEDS = 256;
-const int8_t MAT_PIN = 3;
+const int8_t MAT_PIN = 6;
 
 CRGB leds[NUM_LEDS];
 
@@ -54,7 +54,7 @@ enum mode_t {
 
 const uint8_t days_in_months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-const int pin_mode = 7, pin_plus = 8, pin_minus = 9, pin_high1 = 13, pin_high2 = 4;
+const int pin_mode = 7, pin_plus = 8, pin_minus = 9;
 
 enum mode_t mode = word_disp;
 const uint8_t birth_day = 4, birth_month = 1; // February 5
@@ -141,11 +141,10 @@ void plus(void) {
       nowhour = (nowhour+1)%24;
       break;
     case dig_day: // display di
-      nowday = (nowday+1)%days_in_months[cur_month];
-      if (!nowday) nowday = 1;
+      nowday = (nowday%days_in_months[cur_month])+1;
       break;
     case dig_month: // display me top left, day and month
-      nowmonth = (nowmonth+1)%12;
+      nowmonth = (nowmonth%12)+1;
       if (nowday > days_in_months[nowmonth-1]) {
         nowday = days_in_months[nowmonth-1];
       }
@@ -433,8 +432,11 @@ void display_word(void) {
       case 14: 
         paint_canvas_word(3, 8, 7, date_t); // CATORCE
         break;
-      case 20:
+      case 15:
         paint_canvas_word(7, 9, 6, date_t); // QUINCE
+        break;
+      case 20:
+        paint_canvas_word(2, 10, 6, date_t);
         break;
       default:
         break;
@@ -544,11 +546,6 @@ void setup() {
   pinMode(pin_mode, INPUT);
   pinMode(pin_plus, INPUT);
   pinMode(pin_minus, INPUT);
-  pinMode(pin_high1, OUTPUT);
-  pinMode(pin_high2, OUTPUT);
-
-  digitalWrite(pin_high1, HIGH);
-  digitalWrite(pin_high2, HIGH);
 
   if (!rtc.begin()) {
     paint_canvas_word(0, 3, 5, spec_t); // AUDRI
